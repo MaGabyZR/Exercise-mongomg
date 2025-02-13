6086ee4c14a51c80e00b2020b361b4b0dd09494e
Gabriela Rubio <gabyrubioa@gmail.com>
2025-02-13 11:58:51 -0600

    Querrying part3, index3.js

---
+++ index3.js
@@ -0,0 +1,38 @@
+//connect to MongoDB
+const mongoose = require('mongoose');
+
+//to reference the Mongodb installed in my machine and the database I created.
+mongoose.connect('mongodb://localhost/mongo-exercises'); 
+
+//Define a Schema for the Documents.
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String, 
+    tags: [ String ],
+    date: Date, 
+    isPublished: Boolean,
+    price: Number
+});
+
+//Compile the schema into a model.
+const Course = mongoose.model('Course', courseSchema); //Class Course.
+
+//Query the DB.
+async function getCourses(){
+    return await Course 
+        .find({ isPublished: true })                    //get all published courses, that are USD15 or more or have the word "by" in it.   
+        .or([
+            { price: { $gte: 15 }},
+            { name: /.*by.*/i}
+         ])
+        //.sort({ price: -1 })                               //1st solution sort them by their price in descending order. 
+        .sort('-price')                                     //2nd solution to sort them by their price. 
+        .select(' name author price');                      //pick only their name and author, and show the price.
+}
+//Display them on the console.
+async function run(){
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
